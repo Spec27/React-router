dae9f3d3e6f2dc9cab00d8f09639319608f5fcb1
Spec27 <shkvara27@gmail.com>
2022-07-05 22:44:22 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/React-router/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
