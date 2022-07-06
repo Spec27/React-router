09a430b458d61a02d62a17b83647db61e7660150
Spec27 <shkvara27@gmail.com>
2022-07-06 16:45:49 +0300

    ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
+import { lazy,Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import s from "./App.module.css"
-import AppBar from "./AppBar";
-import HomeVievs from "./viev/HomeViev";
-import AutorViev from "./viev/AutorViev/AutorViev";
-import BooksViev  from "./viev/BooksViev";
-import NotFoundViev from "./viev/NotFoundViev/NotFoundViev";
+import s from "./App.module.css";
 import books from "../data/books.json";
-import BooksDetailsViev from "./viev/BooksDetailsViev";
+import AppBar from "./AppBar";
 
+const HomeVievs = lazy(() => import("./viev/HomeViev"));
+const AutorViev = lazy(() => import("./viev/AutorViev/AutorViev"));
+const BooksViev = lazy(() => import("./viev/BooksViev"));
+const NotFoundViev = lazy(() => import("./viev/NotFoundViev/NotFoundViev"));
+const BooksDetailsViev = lazy(() => import("./viev/BooksDetailsViev"));
 
 
 export const App = () => {
@@ -22,17 +23,18 @@ export const App = () => {
     >
       <>
         <div className={s.container}>
-        
+        <Suspense fallback={<h1>Загружаемо...</h1>}>
           <Routes>
             <Route path="/" element={<AppBar />}>
-            <Route index element={<HomeVievs />} />
-            <Route path="Author" element={<AutorViev />} />
-            <Route path="Books" element={<BooksViev books={books}/>} />
-            <Route path="Books/:bookId" element={<BooksDetailsViev books={books} />} />
-            <Route path="*" element={ <NotFoundViev/>} />
-          </Route>   
-      </Routes>
-        </div>
+               <Route index element={<HomeVievs />} />
+               <Route path="Author" element={<AutorViev />} />
+               <Route path="Books" element={<BooksViev books={books}/>} />
+               <Route path="Books/:bookId" element={<BooksDetailsViev books={books} />} />
+               <Route path="*" element={ <NotFoundViev/>} />
+            </Route>   
+          </Routes>  
+        </Suspense>
+        </div>  
       </>
     </div>
   );
